8ba0e63b11503ffd408972920a0640536bdac5e9
DavidSilTroy <davidsiltroy2008@hotmail.com>
2020-12-23 09:59:56 -0500

    borrado el merge_patient_box.xml y creadors fragment_patients.xml y su respectivo item_patient_box.xml para usarlo de recyclerview en activity_main.xml

--- app/src/main/java/com/example/tests_fisioterapia/MainActivity.kt
+++ app/src/main/java/com/example/tests_fisioterapia/MainActivity.kt
@@ -8,7 +8,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
         // Esto es un comentario
     }
 }
